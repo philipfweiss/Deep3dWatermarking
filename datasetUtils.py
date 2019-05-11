d55ab298ad9662a1a37de077debed18255e17e13
Jonathan Lipman <jlipman500@gmail.com>
2019-05-11 14:36:56 -0700

    add dataset splitter

---
+++ datasetUtils.py
@@ -0,0 +1,34 @@
+import os
+import shutil
+import json
+import argparse
+
+parser = argparse.ArgumentParser(description='Only take shape files with a certain complexity.')
+parser.add_argument('--cutoff', type=int,
+                    help='an integer for the min number of vertices to cut off at')
+
+args = parser.parse_args()
+
+shape_core_dir = "/home/jlipman500/ShapeNetCore.v2"
+new_files_dir = "/home/jlipman500/ShapeNetCore.slice"
+vertice_cutoff = 10000
+if "--cutoff" in args:
+    vertice_cutoff = args["--cutoff"]
+size_of_slice = 0
+total_number = 0
+
+shutil.rmtree(new_files_dir)
+os.makedirs(new_files_dir)
+
+groups = os.listdir(shape_core_dir)
+
+for group in groups:
+    print("slice size: ", size_of_slice, " total number: ", total_number)
+    objects = os.listdir(os.path.join(shape_core_dir, group))
+    for object in objects:
+        with open(os.path.join(shape_core_dir, group, object, "models", "model_normalized.json")) as metadata:
+            data = json.load(metadata)
+            total_number += 1
+            if data["numVertices"] > vertice_cutoff:
+                shutil.copy2(os.path.join(shape_core_dir, group, object), os.path.join(new_files_dir, object))
+                size_of_slice += 1
