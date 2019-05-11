52ccd07deea9612551c382a72ce0458873f98369
Philip Weiss <weissp68@stanford.edu>
2019-05-11 15:29:49 -0700

    [create] basic visualizations

--- utils.py
+++ utils.py
@@ -1,37 +1,54 @@
 import argparse
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+from collections import *
+import matplotlib.pyplot as plt
 
+class RunModel:
+    def __init__(self):
+        self.train_losses = []
+        self.test_losses = {}
 
-def train(args, model, device, train_loader, optimizer, epoch):
-    model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = F.nll_loss(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % args.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
+    def visualize(self):
+        plt.subplot(2,1,1)
+        plt.title('Training Loss')
+        plt.plot(self.train_losses, 'o')
+        plt.xlabel("Iteration")
+        plt.show()
 
-def test(args, model, device, test_loader):
-    model.eval()
-    test_loss = 0
-    correct = 0
-    with torch.no_grad():
-        for data, target in test_loader:
+    def train(self, args, model, device, train_loader, optimizer, epoch):
+        model.train()
+        for batch_idx, (data, target) in enumerate(train_loader):
             data, target = data.to(device), target.to(device)
+            optimizer.zero_grad()
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
-            pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
-            correct += pred.eq(target.view_as(pred)).sum().item()
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            optimizer.step()
+
+            if batch_idx % args.log_interval == 0:
+                self.train_losses.append(loss.item()) #(epoch * args.batch_size + batch_idx, 
+
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader), loss.item()))
+
+    def test(self, args, model, device, test_loader, epoch):
+        model.eval()
+        test_loss = 0
+        correct = 0
+        with torch.no_grad():
+            for data, target in test_loader:
+                data, target = data.to(device), target.to(device)
+                output = model(data)
+                test_loss += F.nll_loss(output, target, reduction='sum').item() # sum up batch loss
+                pred = output.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+                correct += pred.eq(target.view_as(pred)).sum().item()
+        self.test_losses[epoch] = test_loss
+        print(epoch, " reee ", test_loss)
 
 def getargs():
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
