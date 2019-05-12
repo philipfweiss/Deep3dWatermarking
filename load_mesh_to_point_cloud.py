1172af7fad426576041dd5d30e16a731fce25701
jonli123 <johnnyli@stanford.edu>
2019-05-11 20:27:16 -0700

    pyntcloud bs

--- load_mesh_to_point_cloud.py
+++ load_mesh_to_point_cloud.py
@@ -2,13 +2,10 @@ from pyntcloud import PyntCloud
 import pymesh
 import numpy as np
 
-
 def mesh_to_point_cloud(filename):
     anky = PyntCloud.from_file(filename)
-    #anky = pymesh.load_mesh(filename)
-    print(anky)
-    anky_point_cloud = anky.get_sample("mesh_random", n=10000, rgb=False, normals=False)
-    print(anky_point_cloud)
+    anky_point_cloud = anky.get_sample("mesh_random", n=10000, rgb=False, normals=False, as_PyntCloud=True)
+    anky_point_cloud.plot()
 
 def point_cloud_to_mesh(pcl):
     pass
@@ -24,7 +21,6 @@ def convert_to_np_array(filename, new_filename):
     mesh = pymesh.load_mesh(filename)
     vertices = mesh.vertices
     np.save(new_filename, vertices)
-
 
 
 if __name__ == "__main__":
