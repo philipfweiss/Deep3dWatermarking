338e465694b061f6a4a7cdc34340b2e23c9c2556
Jonathan Lipman <jlipman500@gmail.com>
2019-06-02 23:04:25 -0700

    a

--- decoder.py
+++ decoder.py
@@ -43,6 +43,5 @@ class Decoder(nn.Module):
 
         ## Flatten and affine
         x = x.view(x.size(0), -1)
-        x = self.leaky_relu(self.fc1(x))
-        x = self.sigmoid(x)
+        x = self.sigmoid(self.fc1(x))
         return x
