2889e3ee760ff24b5f200b4b299cb1bedaa1104a
Jonathan Lipman <jlipman500@gmail.com>
2019-06-03 19:30:30 -0700

    a

--- Projection2d.py
+++ Projection2d.py
@@ -1,6 +1,6 @@
 import numpy as np
 from numpy import *
-import torch
+# import torch
 import matplotlib.pyplot as plt
 
 
@@ -30,7 +30,7 @@ def capture_picture(p, data, output_height, output_width):
 
     num_found = 0
 
-    dz = -8
+    dz = -0
     ddz = 0.3
     # a = z_dif * dz_end * ddz ** 2
     # dz = a
@@ -90,65 +90,67 @@ ps = [{"x": 16, "y": 40, "z": -10, "phi": 3.875}, {"x": 16, "y": 40, "z": 0, "ph
       {"x": 32, "y": 40, "z": -10, "phi": 3.1}]
 
 
-def convert_to_2d(data):
-    # N x 1 x 64 x 64 x 64 --> N x 6 x 128 x 128
-    results = []
-    for image in data:
-        print("converted image")
-        image = image[0]
-        image_results = []
+# def convert_to_2d(data):
+#     # N x 1 x 64 x 64 x 64 --> N x 6 x 128 x 128
+#     results = []
+#     for image in data:
+#         print("converted image")
+#         image = image[0]
+#         image_results = []
+#
+#         # image_results.append(capture_picture(ps[0], image, 64, 64))
+#         # image_results.append(capture_picture(ps[1], image, 64, 64))
+#         #
+#         # image = image.permute(0, 2, 1)
+#         #
+#         # image_results.append(capture_picture(ps[2], image, 64, 64))
+#         # image_results.append(capture_picture(ps[3], image, 64, 64))
+#         #
+#         # image = image.permute(0, 2, 1)
+#         image = image.permute(2, 1, 0)
+#
+#         image_results.append(capture_picture(ps[4], image, 64, 64))
+#         image_results.append(capture_picture(ps[5], image, 64, 64))
+#         results.append(image_results)
+#
+#     pytens = torch.tensor(results)
+#     return pytens
 
-        # image_results.append(capture_picture(ps[0], image, 64, 64))
-        # image_results.append(capture_picture(ps[1], image, 64, 64))
-        #
-        # image = image.permute(0, 2, 1)
-        #
-        # image_results.append(capture_picture(ps[2], image, 64, 64))
-        # image_results.append(capture_picture(ps[3], image, 64, 64))
-        #
-        # image = image.permute(0, 2, 1)
-        image = image.permute(2, 1, 0)
-
-        image_results.append(capture_picture(ps[4], image, 64, 64))
-        image_results.append(capture_picture(ps[5], image, 64, 64))
-        results.append(image_results)
-
-    pytens = torch.tensor(results)
-    return pytens
-
-# data = array([np.load("/Users/Lipman/Downloads/model_normalized-7.npy")])[0]
+data = array([np.load("/Users/Lipman/Downloads/model_normalized-7.npy")])[0]
 # convert_to_2d(data)
 
-# f, axarr = plt.subplots(2, 2)
-# ax1 = axarr[0, 0]
-# ax1.axis('equal')
-# p = {"x": 0, "y": 45, "z": 0}
-# phi = 0.17222222222222222
-# capture_picture(ax1, p, phi)
-#
-# ax1 = axarr[0, 1]
-# ax1.axis('equal')
-# p = {"x": 0, "y": 45, "z": 0}
-# phi = 0.34444444444444444
-# capture_picture(ax1, p, phi)
-#
-# data = np.swapaxes(data, 1, 2)
-#
-# ax1 = axarr[1, 0]
-# ax1.axis('equal')
-# p = {"x": 0, "y": 32, "z": -10}
-# phi =5.952
-# capture_picture(ax1, p, phi)
-#
-# ax1 = axarr[1, 1]
-# ax1.axis('equal')
-# p = {"x": 0, "y": 32, "z": -10}
-# phi = 0.6888888888888889
-# capture_picture(ax1, p, phi)
-#
-# plt.show()
-#
-# plt.subplots(l, l)
+f, axarr = plt.subplots(3, 2)
+ax1 = axarr[0, 0]
+ax1.axis('equal')
+ax1.imshow(capture_picture({"x": 16, "y": 40, "z": -10, "phi": 3.875}, data, 64, 64))
+
+ax1 = axarr[0, 1]
+ax1.axis('equal')
+ax1.imshow(capture_picture({"x": 16, "y": 40, "z": 0, "phi": 5.425}, data, 64, 64))
+
+data = np.swapaxes(data, 1, 2)
+
+ax1 = axarr[1, 0]
+ax1.axis('equal')
+ax1.imshow(capture_picture(dict(x=10, y=32, z=0, phi=3.875), data, 64, 64))
+
+ax1 = axarr[1, 1]
+ax1.axis('equal')
+ax1.imshow(capture_picture({"x": 10, "y": 32, "z": 0, "phi": 5.425}, data, 64, 64))
+
+data = np.swapaxes(data, 1, 2)
+data = np.swapaxes(data, 0, 2)
+
+ax1 = axarr[2, 1]
+ax1.axis('equal')
+ax1.imshow(capture_picture({"x": 32, "y": 40, "z": -10, "phi": 0}, data, 64, 64))
+
+ax1 = axarr[2, 0]
+ax1.axis('equal')
+ax1.imshow(capture_picture({"x": 32, "y": 40, "z": -10, "phi": 3.1}, data, 64, 64))
+
+plt.show()
+
 
 # good values of phi, x, y: not really, still nead to mess around
 """
