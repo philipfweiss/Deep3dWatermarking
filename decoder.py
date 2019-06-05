5aaa33ce7d30ea610f78c7d644fea000f7f04886
Jonathan Lipman <jlipman500@gmail.com>
2019-06-04 22:03:09 -0700

    a

--- decoder.py
+++ decoder.py
@@ -48,4 +48,6 @@ class Decoder(nn.Module):
         x = x.view(x.size(0), -1)
         x = self.leaky_relu(self.fc1(x))
         x = self.fc2(x)
+        x = self.tanh(x)
+        x = (x + 1) / 2
         return x
